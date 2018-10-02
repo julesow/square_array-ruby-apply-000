389ad6d3e2197c452946c5a5888febd4bb6d9225
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-10-02 13:19:00 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,15 @@
 def square_array(array)
   # your code here
+<<<<<<< HEAD
   ar1=[]
     array.each{ |a| ar1<<(a*a)}
     return ar1
 
+=======
+  array.each do |a|
+  array<<(a*a)
+end
+>>>>>>> c96dc8719973db858dc3cb2466b2b3a14d9ae469
 end
 
 ariba=[1,2,3,4]
